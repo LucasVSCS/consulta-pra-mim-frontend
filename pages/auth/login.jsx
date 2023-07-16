7c5c1680070807f28469a0c6bc307d1ac1cbc4f1
Lucas Vinicius Silva Cerqueira de Souza <lucasvscs@outlook.com>
2023-07-16 20:49:02 -0300

    Ajustes no component da tela de login

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,4 +1,12 @@
-import { Box, Button, CssBaseline, TextField, Typography } from '@mui/material'
+import React from 'react'
+import {
+  Box,
+  Button,
+  CssBaseline,
+  Grid,
+  TextField,
+  Typography
+} from '@mui/material'
 import Image from 'next/image'
 
 import authBackground from '/public/images/auth-background.jpg'
@@ -9,84 +17,88 @@ export default function LoginPage () {
     <div>
       <CssBaseline />
       <Box sx={{ display: 'flex' }}>
-        <Box sx={{ width: '40%' }}>
-          <Box sx={{ position: 'relative', height: '100vh' }}>
-            <Image src={authBackground} layout='fill' objectFit='cover' />
-          </Box>
-        </Box>
-
-        <Box
-          sx={{
-            width: '60%',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            bgcolor: 'black'
-          }}
-        >
-          <Box
-            width={550}
-            height={400}
-            sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center'
-            }}
-          >
-            <Box sx={{ marginBottom: 2 }}>
-              <Image src={logoImage} width={350} height={150} />
+        <Grid container>
+          <Grid item xs={12} md={5}>
+            <Box sx={{ position: 'relative', height: '100vh' }}>
+              <Image src={authBackground} layout='fill' objectFit='cover' />
             </Box>
+          </Grid>
+
+          <Grid item xs={12} md={7}>
             <Box
-              border={2}
-              borderColor={'#e37d7d'}
-              width={550}
-              height={400}
               sx={{
                 display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center'
+                justifyContent: 'center',
+                alignItems: 'center',
+                bgcolor: 'black',
+                height: '100vh'
               }}
             >
-              <Typography
-                variant='h5'
-                color={'white'}
-                sx={{ marginBottom: 2, marginTop: 2 }}
+              <Box
+                width={550}
+                height={400}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center'
+                }}
               >
-                Digite suas credenciais
-              </Typography>
+                <Box sx={{ marginBottom: 2 }}>
+                  <Image src={logoImage} width={350} height={150} />
+                </Box>
+                <Box
+                  border={2}
+                  borderColor={'#e37d7d'}
+                  width={550}
+                  height={400}
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center'
+                  }}
+                >
+                  <Typography
+                    variant='h5'
+                    color={'white'}
+                    sx={{ marginBottom: 2, marginTop: 2 }}
+                  >
+                    Digite suas credenciais
+                  </Typography>
 
-              <TextField
-                label='Nome de usuário'
-                InputLabelProps={{ sx: { color: 'white' } }}
-                sx={{
-                  width: '35vh',
-                  marginBottom: 2,
-                  input: {
-                    background: 'grey'
-                  }
-                }}
-              />
-              <TextField
-                label='Senha'
-                InputLabelProps={{ sx: { color: 'white' } }}
-                sx={{
-                  width: '35vh',
-                  marginBottom: 2,
-                  input: {
-                    background: 'grey'
-                  }
-                }}
-              />
-              <Button
-                variant='contained'
-                color='inherit'
-                sx={{ marginBottom: 2, width: '20vh', height: '4vh' }}
-              >
-                Entrar
-              </Button>
+                  <TextField
+                    label='Nome de usuário'
+                    InputLabelProps={{ sx: { color: 'white' } }}
+                    sx={{
+                      width: '35vh',
+                      marginBottom: 2,
+                      input: {
+                        background: 'grey'
+                      }
+                    }}
+                  />
+                  <TextField
+                    label='Senha'
+                    InputLabelProps={{ sx: { color: 'white' } }}
+                    sx={{
+                      width: '35vh',
+                      marginBottom: 2,
+                      input: {
+                        background: 'grey'
+                      }
+                    }}
+                  />
+                  <Button
+                    variant='contained'
+                    color='inherit'
+                    sx={{ marginBottom: 2, width: '20vh', height: '4vh' }}
+                  >
+                    Entrar
+                  </Button>
+                </Box>
+              </Box>
             </Box>
-          </Box>
-        </Box>
+          </Grid>
+        </Grid>
       </Box>
     </div>
   )
