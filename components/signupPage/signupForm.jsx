9a138ba9772688e4ac8ab797e493f35169528b0a
Lucas Vinicius Silva Cerqueira de Souza <lucasvscs@outlook.com>
2023-07-30 16:37:57 -0300

    Correção na flag de whatsapp do campo telefone

--- components/signupPage/signupForm.jsx
+++ components/signupPage/signupForm.jsx
@@ -148,7 +148,7 @@ export default function SignupForm () {
                 )}
               </Field>
               <Field name='isWhatsapp'>
-                {({ field }) => (
+                {({ field, form }) => (
                   <>
                     <FormControlLabel
                       control={
